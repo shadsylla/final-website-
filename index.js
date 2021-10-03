25fcf18480f0f374aa74cc85cfe66d6e4f3633a3
Shad Sylla <34220082+shadsylla@users.noreply.github.com>
2021-10-03 16:36:25 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function pauseFunc() {
+    document.getElementById('box').style.animationPlayState = "paused";
+    console.log('Paused'),false
+}
+
+function playFunc(){
+    document.getElementById('box').style.animationPlayState = 'running'
+    console.log('Played'),false
+}
+
+function myFunction(x) {
+  x.classList.toggle("change");
+
+}
